ad8dddcf568680094d432e2c2ada0ad2743436d1
shaikarfath <shaik_arfath@columbusstate.edu>
2022-02-09 19:33:29 +0000

    commit

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ let express = require('express');
 let bodyParser = require('body-parser');
 let newsdata = null
 
-var app= express();
+var app = express();
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended : true}));
-app.use( express.static( "public" ) );
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 const port = process.env.PORT || 3000;
 
 
-app.get('/', function(request, response){
-    response.render('main', {newsdata : newsdata});
+app.get('/', function (request, response) {
+    response.render('main', { newsdata: newsdata });
 });
 
 
-app.listen(port, function(){
+app.listen(port, function () {
     console.log('App is running on port 3000!')
 })
