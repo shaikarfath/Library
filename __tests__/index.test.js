8130b8177c5f90f5c880039eb2153f04f6e498b5
shaikarfath <shaik_arfath@columbusstate.edu>
2022-03-31 23:01:16 +0000

    commit

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,31 +9,50 @@ describe('Endpoints', () => {
         try {
             await request.get("/");
             expect(res.text.indexOf("Authors")).toBe(6599);
-            console.log(res.text.indexOf("Titles"));
-            console.log(res.text.indexOf("Popular Series"));
+            expect(res.text.indexOf("Titles")).toBe(6715);
+            expect(res.text.indexOf("Popular Series")).toBe(6830);
+            expect(res.text.indexOf("Antiquarians")).toBe(6952);
+            expect(res.text.indexOf("ABOUT US!")).toBe(8066);
+            expect(res.text.indexOf("Have fun browsing...!!!")).toBe(8509);
+            expect(res.text.indexOf("Team")).toBe(9148);
+            expect(res.text.indexOf("Arfath Shaik")).toBe(9214);
+            expect(res.text.indexOf("Kiranmai Machiraju")).toBe(9288);
+            expect(res.text.indexOf("Contact Us")).toBe(9632);
+            expect(res.text.indexOf("Send")).toBe(10711);
+
         } catch (err) {
         }
     });
     
-    // it('test 1', async () => {
+    
+    
+
+})
+
+// for finding index of the strings - use this as a reference 
+// it('test 1', async done => {
     //     const request = supertest('http://localhost:3000');
     //     await request.get("/")
     //     .expect(200)
-    //     .then((err, res) => {
+    //     .end((err, res) => {
     //         if(err) {
     //             throw err;
     //         } else {
     //             //expect response to ,atch imported constnat
-    //             console.log("Authors");
-    //             console.log(res.text.indexOf("Authors"));
-    //             expect(res.text.indexOf("Authors")).toBe(6599);
+    //             //console.log("Titles");
+    //             console.log(res.text.indexOf("Titles"));
+    //             console.log(res.text.indexOf("Popular Series"));
+    //             console.log(res.text.indexOf("Antiquarians"));
+    //             console.log(res.text.indexOf("ABOUT US!"));
+    //             console.log(res.text.indexOf("Have fun browsing...!!!"));
+    //             console.log(res.text.indexOf("Team"));
+    //             console.log(res.text.indexOf("Arfath Shaik"));
+    //             console.log(res.text.indexOf("Kiranmai Machiraju"));
+    //             console.log(res.text.indexOf("Contact Us"));
+    //             console.log(res.text.indexOf("Send"));
+    //             //expect(res.text.indexOf("Authors")).toBe(6599);
     //             //expect(res.text).toEqual('none');
-    //             //done();
+    //             done();
     //         }
     //     });
     // }, 60000)
-
-
-
-})
-
