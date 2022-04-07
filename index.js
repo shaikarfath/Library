91ed4b024af74c6e7e0e91fea3fc4d71ef712f14
shaikarfath <shaik_arfath@columbusstate.edu>
2022-04-07 22:56:32 +0000

    commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let bodyParser = require('body-parser');
 let booksdata = null
+let length = 0
 const axios = require('axios').default;
 var app = express();
 app.set('view engine', 'ejs');
@@ -9,7 +10,7 @@ app.use(express.static("public"));
 const port = process.env.PORT || 3000;
 
 app.get('/', function (request, response) {
-    response.render('main', {booksdata : booksdata});
+    response.render('main', {booksdata : booksdata, length : length});
 });
 
 app.post('/', function(req,res){
@@ -17,6 +18,7 @@ app.post('/', function(req,res){
     axios.get(`https://www.googleapis.com/books/v1/volumes?q=${req.body.keyword}`)
     .then(function(response){
         booksdata = response.data;      
+        length = response.data.items.length;
                 
          res.redirect('/');
     })
