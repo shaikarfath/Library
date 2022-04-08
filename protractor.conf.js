44a16b7f0568340d03497526876abf319beb213e
JoseJCanedo <canedo_jose@columbusstate.edu>
2022-04-08 02:58:45 +0000

    f

--- protractor.conf.js
+++ protractor.conf.js
@@ -8,6 +8,7 @@ exports.config = {
 
     // Framework to use. Jasmine is recommended.
     framework: 'jasmine',
+    seleniumAddress: 'http://localhost:4444/wd/hub',
     // Spec patterns are relative to the current working directory when protractor is called.
     specs: ['e2e/*spec.js'],
 
