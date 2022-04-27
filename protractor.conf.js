1273f28a472c1447a02f9aae9590b48dfde35375
shaikarfath <shaik_arfath@columbusstate.edu>
2022-04-27 20:19:10 +0000

    commit

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,7 +1,6 @@
 exports.config = {
     directConnect: true,
 
-    // Capabilities to be passed to the webdriver instance.
     capabilities: {
         browserName: 'chrome',
 
@@ -9,13 +8,10 @@ exports.config = {
         args: [ "--disable-gpu" ]
     }
     },
-    // Framework to use. Jasmine is recommended.
+    
     framework: 'jasmine',
-   // seleniumAddress: 'http://localhost:4444/wd/hub',
-    // Spec patterns are relative to the current working directory when protractor is called.
     specs: ['e2e/*spec.js'],
 
-    // Options to be passed to Jasmine.
     jasmineNodeOpts: {
         defaultTimeoutInterval: 100000
     }
